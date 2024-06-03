82c44d871642ce0f684e7f747c4db771d8d0ef00
karracaye <karahcaesantillan@gmail.com>
2024-06-03 18:02:22 +0800

    feat: testimonials

--- pages/index.vue
+++ pages/index.vue
@@ -145,121 +145,150 @@
              </div>
 
 
-             <!-- ITEM 1 RIGHT -->
-             <div class="grid grid-cols-3 pt-3">
+             <!-- ITEM CARD 1 RIGHT -->
+             <div class="grid grid-cols-3 pt-7">
               <div></div>
 
               <div class="flex justify-center items-center relative flex-col gap-4">
                 <p class="text-xs circle-glass">01</p>
-               <div class="bg-white opacity-5 w-px h-48 " />
+               <div class="bg-white opacity-5 w-px vertical" />
               </div>
-
-              <div class="circle-glass">
+              <div class="circle-glass w-full p-8">
                 <!-- .. -->
-                <div class="box ">
-                  <div class="content-box">
-                    <h2 class="text-xs mb-2 opacity-50">02</h2>
-                    <h3 class="text-lg mb-6 opacity-75">WEB DESIGN</h3>
-                    <p class="text-sm mb-5 opacity-30"> </p>
+                <div class="box m-60 max-w-md w-30 sline-in" ref="recentWork">
+                  <div class="content-box w-96 p-3 text-left">
+                    <h2 class="text-xs mb-3 opacity-50">ARE WE COMPATIBLE?</h2>
+                    <h3 class="text-lg mb-2 opacity-75">LET'S CONNECT </h3>
+                    <p class="text-sm mb-5 opacity-30"> Before we start, let's ensure I can assist you effectively. What functionalities do you need for your new website? What's prompting the need for it? Clarifying your objectives helps us tackle challenges and set goals. </p>
+                      <i class="fas fa-phone-alt text-gray opacity-50 mr-4"></i>
+                      <i class="fas fa-video text-gray opacity-50"></i>
                   </div>
                 </div>
               </div>
              </div>
 
-             <!-- ITEM 2 LEFT -->
-             <div class="grid grid-cols-3 pt-3">
-              <div class="circle-glass">
+             <!-- ITEM CARD 2 LEFT -->
+             <div class="grid grid-cols-3 pt-7">
+              <div class="circle-glass w-full p-7">
                 <!-- .. -->
-                <div class="box ">
-                  <div class="content-box">
-                    <h2 class="text-xs mb-2 opacity-50">02</h2>
-                    <h3 class="text-lg mb-6 opacity-75">WEB DESIGN</h3>
-                    <p class="text-sm mb-5 opacity-30"> </p>
+                <div class="box max-w-md w-30 box-card left-box">
+                  <div class="content-box w-96 p-3 text-left ">
+                    <h2 class="text-xs mb-2 opacity-50">PLANNING FOR SUCCESS</h2>
+                    <h3 class="text-lg mb-3 opacity-75">THE CONCEPT & STRATEGY</h3>
+                    <p class="text-sm mb-3 opacity-30"> We collaborate to develop a roadmap that merges your goals with your audience's needs. This roadmap guides the creation of initial wireframes and an interactive prototype, offering a clear vision of the website's layout and user experience.</p>
+                    <div class="logo flex items-center">
+                    <img src="https://i.ibb.co/VNVKrqr/figma-transparent.png" alt="Figma Logo" class="text-gray opacity-50 figma-logo">
+                    <img src="https://i.ibb.co/Rzd9TqL/XD.png" alt="xd Logo" class="text-gray opacity-50 xd-logo">
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="flex justify-center items-center relative flex-col gap-4">
                 <p class="text-xs circle-glass">02</p>
-               <div class="bg-white opacity-5 w-px h-48 " />
+               <div class="bg-white opacity-5 w-px h-48 vertical"/>
               </div>
-              <div></div>
+                <div></div>
              </div>
 
              <!-- ITEM 3 RIGHT -->
-             <div class="grid grid-cols-3 pt-3">
+             <div class="grid grid-cols-3 pt-7">
               <div></div>
 
               <div class="flex justify-center items-center relative flex-col gap-4">
                 <p class="text-xs circle-glass">03</p>
-               <div class="bg-white opacity-5 w-px h-48 " />
+               <div class="bg-white opacity-5 w-px vertical " />
               </div>
 
-              <div class="circle-glass">
+              <div class="circle-glass w-full p-8">
                 <!-- .. -->
-                <div class="box ">
-                  <div class="content-box">
-                    <h2 class="text-xs mb-2 opacity-50">02</h2>
-                    <h3 class="text-lg mb-6 opacity-75">WEB DESIGN</h3>
-                    <p class="text-sm mb-5 opacity-30"> </p>
+                <div class="box m-60 max-w-md w-30">
+                  <div class="content-box w-96 p-3 text-left">
+                    <h2 class="text-xs mb-2 opacity-50">INTERACTIVE PROTOTYPE</h2>
+                    <h3 class="text-lg mb-4 opacity-75">WEB & MOBILE DESIGN</h3>
+                    <p class="text-sm mb-5 opacity-30">Prepare for the transformation. Drawing from your initial concept, I'll fashion a premium screen design finely tuned to your brand. This web design isn't just about standing out—it's about resonating with your audience and delivering seamless user experiences.</p>
                   </div>
                 </div>
               </div>
              </div>
 
              <!-- ITEM 4 LEFT -->
-             <div class="grid grid-cols-3 pt-3">
-              <div class="circle-glass">
+             <div class="grid grid-cols-3 pt-7">
+              <div class="circle-glass w-full p-7">
                 <!-- .. -->
-                <div class="box ">
-                  <div class="content-box">
-                    <h2 class="text-xs mb-2 opacity-50">02</h2>
-                    <h3 class="text-lg mb-6 opacity-75">WEB DESIGN</h3>
-                    <p class="text-sm mb-5 opacity-30"> </p>
+                <div class="box max-w-md w-30 box-card left-box">
+                    <div class="content-box w-96 p-3 text-left ">
+                      <h2 class="text-xs mb-2 opacity-50">CROSS-PLATFORM SOLUTION</h2>
+                      <h3 class="text-lg mb-3 opacity-75">DEVELOPMENT</h3>
+                      <p class="text-sm mb-3 opacity-30">Now, we infuse vitality into your refined design. Expect a bespoke website crafted with a modular web design system and seamless CMS integration. Elevate your online presence with captivating animations that distinguish you from the mundane competition.</p>
+                      <div class="logo flex items-center">
+                      <img src="https://i.ibb.co/PQdhPrv/nuxt-logo-1-CCC5-F38-FD-seeklogo-com.png" alt="nuxt Logo" class="text-gray opacity-50 nuxt-logo mr-2">
+                      <img src="https://i.ibb.co/D7b0r5J/logo.png" alt="vue Logo" class="text-gray opacity-50 vue-logo mr-1">
+                      <img src="https://i.ibb.co/cLmLTZ0/screenshot-361-removebg-preview.png" alt="tailwind Logo" class="text-gray opacity-50 tailwind-logo mr-1">
+                      <img src="https://i.ibb.co/jbLgzmZ/react-logo.png" alt="react Logo" class="text-gray opacity-50 react-logo mr-2">
+                    </div>
                   </div>
                 </div>
               </div>
               <div class="flex justify-center items-center relative flex-col gap-4">
                 <p class="text-xs circle-glass">04</p>
-               <div class="bg-white opacity-5 w-px h-48 " />
+               <div class="bg-white opacity-5 w-px vertical "/>
               </div>
               <div></div>
              </div>
 
-              <!-- ITEM 3 RIGHT -->
-              <div class="grid grid-cols-3 pt-3">
-              <div></div>
+              <!-- ITEM 5 RIGHT -->
+              <div class="grid grid-cols-3 pt-7">
+                <div></div>
 
-              <div class="flex justify-center items-center relative flex-col gap-4">
-                <p class="text-xs circle-glass">05</p>
-               <div class="bg-white opacity-5 w-px h-48 " />
-              </div>
+                  <div class="flex justify-center items-center relative flex-col gap-4">
+                    <p class="text-xs circle-glass">05</p>
+                  <div class="bg-white opacity-5 w-px vertical " />
+                  </div>
 
-              <div class="circle-glass">
-                <!-- .. -->
-                <div class="box ">
-                  <div class="content-box">
-                    <h2 class="text-xs mb-2 opacity-50">02</h2>
-                    <h3 class="text-lg mb-6 opacity-75">WEB DESIGN</h3>
-                    <p class="text-sm mb-5 opacity-30"> </p>
+                <div class="circle-glass">
+                  <!-- .. -->
+                  <div class="box m-60 max-w-md w-30">
+                    <div class="content-box w-96 p-3 text-left">
+                      <h2 class="text-xs mb-2 opacity-50">READY TO GO</h2>
+                      <h3 class="text-lg mb-3 opacity-75">WEBSITE ONBOARDING</h3>
+                      <p class="text-sm mb-5 opacity-30">Begin a tailored Framer workshop to equip yourself with the abilities to promptly and easily modify your recently launched website. Dive into personalized video tutorials for quick access whenever necessary. Our user-friendly system ensures that editing content on your website feels natural and intuitive.</p>
+                    </div>
                   </div>
                 </div>
               </div>
-             </div>
-
-
             </div>
           </div>
-        <!-- add here ..  --> 
+            <div class="text-white font-extralight flex items-center justify-center text-center mt-1 p-10">
+              <div>
+                <p class="text-sm opacity-30 ">TESTIMONIALS</p>
+                <p class="text-7xl opacity-75 p-5"> What my</p>
+                <p class="text-6xl opacity-75 font-serif steps">clients say</p>
+                <p class="text-lg opacity-30 p-6">Discover what my clients have to share about their experience working with <br> me and the outcomes I assisted them in accomplishing.</p>
+              </div>
+            </div>
+              <div class="box max-w-md h-96 w-96 right-box">
+                <div class="box h-96 w-96 right-two-box">
+                </div>
+                <div class="testimonials h-20 w-80 justify-center items-center  ">
+                  <div class="text-xs circle-glass-comment p-6 pt-15"> </div>
+                  <p class="text-xs text-white">This is a customer testimonial. It could be a comment about the product or service.</p>
+                </div>   
+            </div>
+              
+            
+            
+        </div>
+        
       </div>
-    </div>
   </template>
   
   <script setup lang="ts">
   import { ref, onMounted, onUnmounted } from 'vue';
   import LogoCarousel from '~/pages/LogoCarousel.vue';
   
+
   const recentWorkVisible = ref(false);
-  
+
   const handleScroll = () => {
     const recentWorkEl = document.getElementById('recentWork');
     if (recentWorkEl) {
@@ -270,6 +299,7 @@
         window.removeEventListener('scroll', handleScroll);
       }
     }
+    
   };
 
   onMounted(() => {
