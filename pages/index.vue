b1063d98343cdeedf7e724f60531bb15dcb706d0
karracaye <karahcaesantillan@gmail.com>
2024-06-04 13:38:18 +0800

    feat: finishing the testimonial section

--- pages/index.vue
+++ pages/index.vue
@@ -264,6 +264,23 @@
                 <p class="text-7xl opacity-75 p-5"> What my</p>
                 <p class="text-6xl opacity-75 mb-3 font-serif steps">clients say</p>
                 <p class="text-lg opacity-30 p-6">Unlock the power of real stories. Discover firsthand experiences and successes <br> shared by my valued clients, highlighting the transformative impact of our collaboration.</p>
+                <div class="flex items-center justify-center">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 mr-5 text-yellow-500">
+                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 mr-5 text-yellow-500">
+                      <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 mr-5 text-yellow-500">
+                      <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 mr-5 text-yellow-500">
+                      <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6 mr-5 opacity-30">
+                      <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z" clip-rule="evenodd" />
+                  </svg>
+                </div>
               </div>
             </div>
             <div class="new-container">
@@ -271,23 +288,33 @@
                   <div class="box max-w-md h-96 w-96 right-box">
                     <div class="box h-96 w-96 right-two-box">
                     </div>
-                    <div class="testimonials h-20 w-80 justify-center items-center">
+                    <div class="testimonials h-30 w-96 justify-center items-center">
                       <div class="testimonial-content flex items-center">
                         <div class="testimonial-image m-1">
                           <img src="../public/img/pf-4.jpeg" alt="Testimonial Image">
                         </div>
-                        <div class="testimonial-text">
-                          <p class="text-xs text-white">This is a customer testimonial. It could be a comment about the product or service.</p>
+                        <div class="testimonial-text flex justify-between items-center w-full">
+                          <p class="text-xs text-white font-thin opacity-70">This is a customer testimonial. It could be a comment about the product or service.</p>
+                          <div class="like-icon p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 w-6 h-6 text-white opacity-50">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+                            </svg>
+                          </div>
                         </div>
                       </div>
                     </div>
-                    <div class="test bottom h-20 w-80 justify-center items-center">
+                    <div class="test bottom h-[6rem] w-96 justify-center items-center">
                       <div class="testimonial-content flex items-center">
                         <div class="testimonial-image m-1">
                           <img src="../public/img/pf.jpeg" alt="Testimonial Image">
                         </div>
-                      <div class="testimonial-text">
-                        <p class="text-xs text-white">This is another customer testimonial positioned at the bottom.</p>
+                      <div class="testimonial-text flex justify-between items-center w-full">
+                        <p class="text-xs text-white font-thin opacity-70">From the first conversation and idea exchanges on what and how to develop a website to our endless list of needs.</p>
+                        <div class="like-icon p-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 w-6 h-6 text-white opacity-50">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.25c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 0 1 2.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 0 0 .322-1.672V2.75a.75.75 0 0 1 .75-.75 2.25 2.25 0 0 1 2.25 2.25c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282m0 0h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 0 1-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 0 0-1.423-.23H5.904m10.598-9.75H14.25M5.904 18.5c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 0 1-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 9.953 4.167 9.5 5 9.5h1.053c.472 0 .745.556.5.96a8.958 8.958 0 0 0-1.302 4.665c0 1.194.232 2.333.654 3.375Z" />
+                            </svg>
+                          </div>
                       </div>
                     </div>
                   </div>
@@ -295,7 +322,7 @@
               </div>
               <div class="description-testimonials ml-20">
                 <div class="rounded-sm p-5">
-                  <p class="text-white text-sm">Discover what my clients have to share about their experience working with me and the outcomes I assisted them in accomplishing. From overcoming challenges to achieving their goals, their stories highlight the impact of our collaboration and the value of my expertise. </p>
+                  <p class="description font-thin text-white text-lg opacity-30">Discover firsthand testimonials from my clients, showcasing their experiences collaborating with me and the remarkable outcomes we achieved together. Delve into narratives ranging from triumphs over challenges to the realization of their most ambitious goals. Through these stories, witness the profound impact of our collaboration and the invaluable contributions of my expertise, illustrating the transformative journey we embarked upon together.</p>
               </div>
             </div>
           </div>
