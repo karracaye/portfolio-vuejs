7289b7b96847af3aa08cf6fd35e9900aa72dc3e3
karracaye <karahcaesantillan@gmail.com>
2024-06-06 17:35:46 +0800

    ops

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,1 +1,1 @@
-export { default } from "file:///Users/apple/Documents/portfolio-vuejs/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
+export { default } from "file:///Users/karracaye/Desktop/code/portfolio-vuejs/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
