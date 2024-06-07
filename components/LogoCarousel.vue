97f281af8099d8dbc0a99d8a5056bba9e27cc397
karracaye <karahcaesantillan@gmail.com>
2024-06-07 09:58:06 +0800

    Innovex

--- components/LogoCarousel.vue
+++ components/LogoCarousel.vue
@@ -12,14 +12,14 @@
         <img src="../public/img/GPA.png" class="logo-animation small-logo" alt="Logo 3" style="width: 90px; height: 90px; margin-right: 100px;">
         <img src="../public/img/Lawsys.png" class="logo-animation small-logo" alt="Logo 4" style="width: 70px; height: 70px; margin-right: 110px;">
         <img src="../public/img/75-anniv-logo.png" class="logo-animation small-logo" alt="Logo 5" style="width: 80px; height: 80px; margin-right: 110px;">
-        <img src="../public/INNOVEX 1.png" class="logo-animation small-logo" alt="Logo 6" style="width: 80px; height: 80px; margin-right: 110px;">
+        <img src="../public/img/INNOVEX 1.png" class="logo-animation small-logo" alt="Logo 6" style="width: 80px; height: 80px; margin-right: 110px;">
         <!-- Duplicate -->
         <img src="../public/img/white-intelliseven.png" class="logo-animation small-logo" alt="Logo 1" style="width: 190px; height: 50px; margin-right: 110px;">
         <img src="../public/img/hotel carlito.png" class="logo-animation small-logo" alt="Logo 2" style="width: 110px; height: 50px; margin-right: 110px;">
         <img src="../public/img/GPA.png" class="logo-animation small-logo" alt="Logo 3" style="width: 90px; height: 90px; margin-right: 100px;">
         <img src="../public/img/Lawsys.png" class="logo-animation small-logo" alt="Logo 4" style="width: 70px; height: 70px; margin-right: 110px;">
         <img src="../public/img/75-anniv-logo.png" class="logo-animation small-logo" alt="Logo 5" style="width: 80px; height: 80px; margin-right: 110px;">
-        <img src="../public/INNOVEX 1.png" class="logo-animation small-logo" alt="Logo 6" style="width: 80px; height: 80px; margin-right: 110px;">
+        <img src="../public/img/INNOVEX 1.png" class="logo-animation small-logo" alt="Logo 6" style="width: 80px; height: 80px; margin-right: 110px;">
         
     </div>
 </template>
