6e7a405fe43b43729f9d7eb8b3fcbc688aee4497
karracaye <karahcaesantillan@gmail.com>
2024-06-08 21:31:46 +0800

    feat: add pictures to the service.

--- pages/moredetails.vue
+++ pages/moredetails.vue
@@ -31,7 +31,7 @@
             <div class="details-content text-left flex flex-col items-left justify-left mb-60">
                 <p class="text-white text-sm font-thin mb-5">WEB DESIGN</p>
                 <p class="text-white text-6xl font-thin opacity-50 mb-10">Transforming Your Ideas into Reality</p>
-                <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="mockup3 w-full mb-[8rem]">
+                <img  src="../public/img/MOCKUP-2.png" alt="Document Photo 1" class="mockup3 w-full mb-[8rem]">
                 
                 <div class="flex flex-col gap-5">
                     <div class="grid grid-cols-4 mt-[-1rem] gap-4">
@@ -66,17 +66,78 @@
                             <p class="text-sm opacity-30 mb-5">With an interactive prototype, you'll have the ability to test your website's functionality before it goes live. This will ensure that your website's design and user experience are optimized for your audience's needs and preferences.</p>
                         </div>
                     </div>
-                    
-                
-                
-                
-                
-                
                 </div>
             </div>    
         </div>
-        
-      
+        <div class=" flex justify-center items-center mt-[-6rem]">
+            <div class="details-content text-left flex flex-col items-left justify-left mb-60">
+                <p class="text-white text-sm font-thin mb-5">DEVELOPMENT</p>
+                <p class="text-white text-6xl font-thin opacity-50 mb-10">Developing High-Performance Websites and Web Applications</p>
+                <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="mockup3 w-full mb-[8rem]">
+                
+                <div class="flex flex-col gap-5">
+                    <div class="grid grid-cols-4 mt-[-1rem] gap-4">
+                    <!-- Left side -->
+                        <div class="text-white text-sm font-thin text-left">
+                            <p class="text-sm mb-3 opacity-70">FRAMER</p>
+                        </div>
+                    <!-- Right side -->
+                        <div class=" text-white text-sm text-left font-thin col-span-3">
+                            <p class="text-sm opacity-30 mb-2">I specialize in developing web applications using Framer. From custom animations to complex interactions, I bring your web app to life.</p>
+                        </div>
+                    </div>
+                    <hr class="opacity-20 mb-10">
+                    <div class="grid grid-cols-4 mt-[-1rem] gap-4">
+                    <!-- Left side -->
+                        <div class="text-white text-sm font-thin text-left">
+                            <p class="text-sm mb-3 opacity-70"> INTEGRATION</p>
+                        </div>
+                    <!-- Right side -->
+                        <div class=" text-white text-sm text-left font-thin col-span-3">
+                            <p class="text-sm opacity-30 mb-2">I can help you streamline your content management process by integrating your website. Say goodbye to manual updates and hello to efficiency.</p>
+                        </div>
+                    </div>
+                    <hr class="opacity-20 mb-10">
+                    <div class="grid grid-cols-4 mt-[-1rem] gap-4">
+                    <!-- Left side -->
+                        <div class="text-white text-sm font-thin text-left">
+                            <p class="text-sm mb-3 opacity-70">WEB DESIGN SYSTEM</p>
+                        </div>
+                    <!-- Right side -->
+                        <div class=" text-white text-sm text-left font-thin col-span-3">
+                            <p class="text-sm opacity-30 mb-5">I use a modular design approach to create a web design system that ensures consistency throughout your website. This results in a professional and cohesive online presence.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </div>
+        <div class="px-[80px]">
+            <p class="text-6xl mb-3 opacity-70 text-white">MY PAST PROJECTS</p>
+            <p class="text-lg mb-10 text-white opacity-30">I use a modular design approach to create a web design system that ensures </p>
+
+            <div class="grid grid-cols-4 gap-4 mb-4 galleries">
+                <div class="h-[250px] overflow-hidden rounded-lg">
+                    <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="w-100">
+                </div>
+                <div class="h-[250px] overflow-hidden rounded-lg">
+                    <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="w-100">
+                </div>
+                <div class="col-span-2 h-[250px] overflow-hidden rounded-lg">
+                    <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="w-100">
+                </div>
+            </div>
+            <div class="grid grid-cols-5 gap-4 galleries">
+                <div class="col-span-2 h-[250px] overflow-hidden rounded-lg">
+                    <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="w-100">
+                </div>
+                <div class="h-[250px] overflow-hidden rounded-lg">
+                    <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="w-100">
+                </div>
+                <div class="col-span-2 h-[250px] overflow-hidden rounded-lg">
+                    <img  src="../public/img/MOCKUP-3.jpg" alt="Document Photo 1" class="w-100">
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </template>
